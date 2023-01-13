bc065d7edb5355c5afd4258ba097b092a0debdfa
kot19746 <120244290+kot19746@users.noreply.github.com>
2023-01-13 16:48:01 +0100

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,17 @@
+﻿/*Задание 1.
+Дан массив. Нужно вывести в консоль количество чётных и нечётных 
+элементов в массиве.*/
+
+const myArr = [2, 3, 4, 5, null]
+
+function oddEvenNum() {
+  const odd = myArr.filter(elem => elem % 2 !== 0)
+  const even = myArr.filter(elem => elem % 2 === 0)
+  const simbol = myArr.filter(elem => elem === null)
+  
+  console.log(odd.length +' нечётных элемента(ов)')
+  console.log(even.length +' чётных элемента(ов)')
+  console.log(simbol.length +' нулевой элемент')
+}
+  oddEvenNum(myArr)
+
