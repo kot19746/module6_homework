a5f6a2cbc20bdd0ff9da7a1f518c35a9669d4469
kot19746 <120244290+kot19746@users.noreply.github.com>
2023-01-14 11:04:40 +0100

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -2,16 +2,20 @@
 Дан массив. Нужно вывести в консоль количество чётных и нечётных 
 элементов в массиве.*/
 
-const myArr = [2, 3, 4, 5, null]
+const myArr = [0, 2, 3, 4, 5, null]
 
 function oddEvenNum() {
-  const odd = myArr.filter(elem => elem % 2 !== 0)
-  const even = myArr.filter(elem => elem % 2 === 0)
-  const simbol = myArr.filter(elem => elem === null)
+const numberArray = myArr.filter(elem => typeof  elem === 'number' && !isNaN(elem))
+
+//const otherElements = arr.filter(elem => typeof  elem !== 'number') // Это не обязательно, просто добавил для наглядности что сида попадают прочие элементы и символы
+
+const odd = numberArray.filter(elem => elem % 2 !== 0)
+const even = numberArray.filter(elem => elem % 2 === 0)
+const zeroElements = numberArray.filter(elem => elem === 0) 
   
-  console.log(odd.length +' нечётных элемента(ов)')
-  console.log(even.length +' чётных элемента(ов)')
-  console.log(simbol.length +' нулевой элемент')
+ console.log(zeroElements.length +' нулевых элементов')
+console.log(odd.length +' нечётных элемента(ов)')
+console.log(even.length +' чётных элемента(ов)')
 }
   oddEvenNum(myArr)
 
